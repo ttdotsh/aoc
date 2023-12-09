46d9fac4e647a4f45046c64c0cb1a298f653460c
timTonelli <102175985+timTonelli@users.noreply.github.com>
2023-12-09 16:53:46 -0500

    trim down tests for maybe chain

--- 2023/src/bin/day3/maybe_chain.rs
+++ 2023/src/bin/day3/maybe_chain.rs
@@ -37,72 +37,66 @@ impl<I: Iterator> ChainOption for I {}
 
 #[cfg(test)]
 mod test {
-    use std::slice::Iter;
+    use std::vec::IntoIter;
 
     use super::*;
 
     #[test]
     fn test_maybe_chain_with_some() {
-        let nums = vec![1, 2, 3];
-        let more_nums = vec![4, 5, 6];
+        let nums = vec![1, 2];
+        let more_nums = vec![3, 4];
 
-        let mut all_nums = nums.iter().maybe_chain(Some(more_nums.iter()));
+        let mut all_nums = nums.into_iter().maybe_chain(Some(more_nums.into_iter()));
 
-        assert_eq!(all_nums.next(), Some(&1));
-        assert_eq!(all_nums.next(), Some(&2));
-        assert_eq!(all_nums.next(), Some(&3));
-        assert_eq!(all_nums.next(), Some(&4));
-        assert_eq!(all_nums.next(), Some(&5));
-        assert_eq!(all_nums.next(), Some(&6));
+        assert_eq!(all_nums.next(), Some(1));
+        assert_eq!(all_nums.next(), Some(2));
+        assert_eq!(all_nums.next(), Some(3));
+        assert_eq!(all_nums.next(), Some(4));
+        assert_eq!(all_nums.next(), None);
     }
 
     #[test]
     fn test_maybe_chain_with_none() {
-        let nums = vec![1, 2, 3];
+        let nums = vec![1, 2];
 
-        let mut all_nums = nums.iter().maybe_chain::<Iter<'_, i32>>(None);
+        let mut all_nums = nums.into_iter().maybe_chain::<IntoIter<i32>>(None);
 
-        assert_eq!(all_nums.next(), Some(&1));
-        assert_eq!(all_nums.next(), Some(&2));
-        assert_eq!(all_nums.next(), Some(&3));
+        assert_eq!(all_nums.next(), Some(1));
+        assert_eq!(all_nums.next(), Some(2));
         assert_eq!(all_nums.next(), None);
     }
 
     #[test]
     fn test_maybe_chain_with_some_then_none() {
-        let nums = vec![1, 2, 3];
-        let more_nums = vec![4, 5, 6];
+        let nums = vec![1, 2];
+        let more_nums = vec![3, 4];
 
         let mut all_nums = nums
-            .iter()
-            .maybe_chain(Some(more_nums.iter()))
-            .maybe_chain::<MaybeChain<Iter<'_, i32>, Iter<'_, i32>>>(None);
+            .into_iter()
+            .maybe_chain(Some(more_nums.into_iter()))
+            .maybe_chain::<MaybeChain<IntoIter<i32>, IntoIter<i32>>>(None);
 
-        assert_eq!(all_nums.next(), Some(&1));
-        assert_eq!(all_nums.next(), Some(&2));
-        assert_eq!(all_nums.next(), Some(&3));
-        assert_eq!(all_nums.next(), Some(&4));
-        assert_eq!(all_nums.next(), Some(&5));
-        assert_eq!(all_nums.next(), Some(&6));
+        assert_eq!(all_nums.next(), Some(1));
+        assert_eq!(all_nums.next(), Some(2));
+        assert_eq!(all_nums.next(), Some(3));
+        assert_eq!(all_nums.next(), Some(4));
         assert_eq!(all_nums.next(), None);
     }
 
     #[test]
     fn test_maybe_chain_with_none_then_some() {
-        let nums = vec![1, 2, 3];
-        let more_nums = vec![4, 5, 6];
+        let nums = vec![1, 2];
+        let more_nums = vec![3, 4];
 
         let mut all_nums = nums
-            .iter()
-            .maybe_chain::<Iter<'_, i32>>(None)
-            .maybe_chain(Some(more_nums.iter()));
+            .into_iter()
+            .maybe_chain::<IntoIter<i32>>(None)
+            .maybe_chain(Some(more_nums.into_iter()));
 
-        assert_eq!(all_nums.next(), Some(&1));
-        assert_eq!(all_nums.next(), Some(&2));
-        assert_eq!(all_nums.next(), Some(&3));
-        assert_eq!(all_nums.next(), Some(&4));
-        assert_eq!(all_nums.next(), Some(&5));
-        assert_eq!(all_nums.next(), Some(&6));
+        assert_eq!(all_nums.next(), Some(1));
+        assert_eq!(all_nums.next(), Some(2));
+        assert_eq!(all_nums.next(), Some(3));
+        assert_eq!(all_nums.next(), Some(4));
         assert_eq!(all_nums.next(), None);
     }
 }
