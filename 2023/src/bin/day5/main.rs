e06d5ac898683563ae9699f3d06a9cfce4c473de
timTonelli <102175985+timTonelli@users.noreply.github.com>
2023-12-12 23:09:49 -0500

    add brute force solution for part 2

--- 2023/src/bin/day5/main.rs
+++ 2023/src/bin/day5/main.rs
@@ -2,14 +2,16 @@ mod map;
 
 use map::Mapper;
 
-use aoc_2023::Solution;
+use aoc_2023::{show_solutions, Solution};
 use itertools::Itertools;
 
 fn main() {
     let input = include_str!("../../../puzzle-input/day5.txt");
 
     let part1 = PartOne::new(input);
-    println!("{}", part1.solve());
+    let part2 = PartTwo::new(input);
+
+    show_solutions(part1, part2);
 }
 
 struct PartOne<'a>(&'a str);
@@ -48,6 +50,53 @@ impl Solution for PartOne<'_> {
         seeds
             .into_iter()
             .map(|seed| mapper.find_destination(seed))
+            .min()
+            .unwrap()
+    }
+}
+
+struct PartTwo<'a>(&'a str);
+
+impl<'a> PartTwo<'a> {
+    fn new(input: &'a str) -> Self {
+        Self(input)
+    }
+}
+
+impl Solution for PartTwo<'_> {
+    type Output = u64;
+
+    fn solve(&self) -> Self::Output {
+        let mut lines = self.0.trim().lines();
+
+        let seed_ranges = lines
+            .next()
+            .unwrap()
+            .replace("seeds:", "")
+            .split_whitespace()
+            .map(|str| str.parse::<u64>().unwrap())
+            .tuples()
+            .collect::<Vec<(u64, u64)>>();
+
+        let mapper = lines
+            .map(|line| line.trim())
+            .group_by(|line| !line.is_empty())
+            .into_iter()
+            .filter_map(|(key, mut group)| match key {
+                true => Some(group.join("\n")),
+                false => None,
+            })
+            .filter_map(|str| str.parse().ok())
+            .collect::<Mapper>();
+
+        seed_ranges
+            .into_iter()
+            .inspect(|tuple| println!("{:?}", tuple))
+            .filter_map(|(start, length)| {
+                (start..start + length)
+                    .map(|seed| mapper.find_destination(seed))
+                    .min()
+            })
             .min()
             .unwrap()
     }
@@ -98,6 +147,51 @@ mod test {
         let expected = 35;
 
         let solution = PartOne::new(input).solve();
+
+        assert_eq!(solution, expected);
+    }
+
+    #[test]
+    fn test_sample_input_part2() {
+        let input = "
+            seeds: 79 14 55 13
+
+            seed-to-soil map:
+            50 98 2
+            52 50 48
+
+            soil-to-fertilizer map:
+            0 15 37
+            37 52 2
+            39 0 15
+
+            fertilizer-to-water map:
+            49 53 8
+            0 11 42
+            42 0 7
+            57 7 4
+
+            water-to-light map:
+            88 18 7
+            18 25 70
+
+            light-to-temperature map:
+            45 77 23
+            81 45 19
+            68 64 13
+
+            temperature-to-humidity map:
+            0 69 1
+            1 0 69
+
+            humidity-to-location map:
+            60 56 37
+            56 93 4
+        ";
+
+        let expected = 46;
+
+        let solution = PartTwo::new(input).solve();
 
         assert_eq!(solution, expected);
     }
